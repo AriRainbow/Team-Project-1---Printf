58027b374f00f8eeed86f35aeaec1bc75c3550d3
AriRainbow <mariel.williams@atlasschool.com>
2024-06-28 00:44:17 +0000

    Set up printf.c

---
+++ printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed, excluding null byte
+ */
+
+int _printf(const char *format, ...)
+{
+	xxxx
+}

2bc8e93e5512a9c75595c1007a9ce9fdf4d5f125
AriRainbow <mariel.williams@atlasschool.com>
2024-06-28 00:23:32 +0000

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,7 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 /**
- * utils.h
- *
  * Header file containing utility functions for project Atlas-Printf.
  * Includes functions for common opperations and helper functions used
  * across different modules of the project.
  */
+
+/* insert prototypes here */
+
+#endif

5e6d3a57654eb0d154a62b9d3166b9323ba08ab3
AriRainbow <mariel.williams@atlasschool.com>
2024-06-28 15:37:10 +0000

    initialize variables

--- printf.c
+++ printf.c
@@ -10,5 +10,16 @@
 
 int _printf(const char *format, ...)
 {
-	xxxx
+	va_list args; /* va_list to hold variable arguments */
+	int count = 0; /* counter for number characters printed */
+	char *str_arg; /* string argument for %s specifier */
+	char char_arg; /* character argument for %c specifier */
+
+	/* initialize va_list with format argument */
+	va_start(args, format);
+
+	/* code */
+
+	va_end(args); /* clean up va_list */
+	return (count);
 }

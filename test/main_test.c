8e0a7239cf13d57416b941e0c8cfce6cd26bb916
AriRainbow <mariel.williams@atlasschool.com>
2024-06-28 02:09:53 +0000

    Setting up test directory

---
+++ test/main_test.c
@@ -0,0 +1,13 @@
+#include "../main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	-printf("Characters: [%c]\n", "H");
+	_printf("String: [%s]\n", "Hello, World!");
+	_printf("Percent: [%%]\n");
+	_printf("Integer: [%d]\n", 42);
+	_printf("Negative integer: [%d]\n", -42);
+
+	return (0);
+}

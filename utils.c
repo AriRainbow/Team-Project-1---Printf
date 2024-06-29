5d11be359f7065662c1f9ada6f690205efb76a61
AriRainbow <mariel.williams@atlasschool.com>
2024-06-29 01:55:24 +0000

    printf

--- utils.c
+++ utils.c
@@ -3,6 +3,8 @@
 
 void print_number(int n, int *count)
 {
+	char digit;
+
 	if (n < 0)
 	{
 		write(1, "-", 1);
@@ -12,16 +14,18 @@ void print_number(int n, int *count)
 
 	if (n / 10)
 		print_number(n / 10, count);
-	char digit = n % 10 + '0';
+	digit = n % 10 + '0';
 	write(1, &digit, 1);
 	(*count)++;
 }
 
 void print_unsigned(unsigned int n, int *count)
 {
+	char digit;
+
 	if (n / 10)
 		print_unsigned(n / 10, count);
-	char digit = n % 10 + '0';
+	digit = n % 10 + '0';
 	write(1, &digit, 1);
 	(*count)++;
 }

dd05fd473e4ba4413c7ab99d27a39a25f1c536bd
AriRainbow <mariel.williams@atlasschool.com>
2024-06-29 01:50:25 +0000

    printf

---
+++ utils.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include "main.h"
+
+void print_number(int n, int *count)
+{
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		(*count)++;
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10, count);
+	char digit = n % 10 + '0';
+	write(1, &digit, 1);
+	(*count)++;
+}
+
+void print_unsigned(unsigned int n, int *count)
+{
+	if (n / 10)
+		print_unsigned(n / 10, count);
+	char digit = n % 10 + '0';
+	write(1, &digit, 1);
+	(*count)++;
+}
